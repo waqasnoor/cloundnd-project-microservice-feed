60a9f590d12e77405abf24bedcaba058d8cf531f
Waqsa Noor <waqasn2c@gmail.com>
2020-12-19 21:22:45 +0500

    Initial Commit

---
+++ src/controllers/v0/index.router.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response } from "express";
+import { FeedRouter } from "./feed/routes/feed.router";
+
+const router: Router = Router();
+
+router.use("/feed", FeedRouter);
+
+export const IndexRouter: Router = router;
