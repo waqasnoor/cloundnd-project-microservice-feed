5ff3bc89ca170e5035c07a46e9e73ac52936c464
Waqsa Noor <waqasn2c@gmail.com>
2020-12-20 19:49:08 +0500

    refactor: routes and styling fix
    fixed feed router and some styling

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -1,83 +1,90 @@
-import {Router, Request, Response} from 'express';
-import {FeedItem} from '../models/FeedItem';
-import {NextFunction} from 'connect';
-import * as jwt from 'jsonwebtoken';
-import * as AWS from '../../../../aws';
-import * as c from '../../../../config/config';
+import { Router, Request, Response } from "express";
+import { FeedItem } from "../models/FeedItem";
+import { NextFunction } from "connect";
+import * as jwt from "jsonwebtoken";
+import * as AWS from "../../../../aws";
+import * as c from "../../../../config/config";
 
 const router: Router = Router();
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
   if (!req.headers || !req.headers.authorization) {
-    return res.status(401).send({message: 'No authorization headers.'});
+    return res.status(401).send({ message: "No authorization headers." });
   }
-
-  const tokenBearer = req.headers.authorization.split(' ');
+  const tokenBearer = req.headers.authorization.split(" ");
   if (tokenBearer.length != 2) {
-    return res.status(401).send({message: 'Malformed token.'});
+    return res.status(401).send({ message: "Malformed token." });
   }
 
   const token = tokenBearer[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
     if (err) {
-      return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate." });
     }
+
     return next();
   });
 }
 
 // Get all feed items
-router.get('/', async (req: Request, res: Response) => {
-  const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
-  items.rows.map((item) => {
-    if (item.url) {
-      item.url = AWS.getGetSignedUrl(item.url);
+router
+  .route("/")
+  .get(async (req: Request, res: Response) => {
+    console.log("feed 1 get called");
+    const items = await FeedItem.findAndCountAll({ order: [["id", "DESC"]] });
+    items.rows.map((item) => {
+      if (item.url) {
+        item.url = AWS.getGetSignedUrl(item.url);
+      }
+    });
+    res.send(items);
+  })
+  .post(requireAuth, async (req: Request, res: Response) => {
+    console.log("feed 1 post called");
+    const caption = req.body.caption;
+    const fileName = req.body.url; // same as S3 key name
+
+    if (!caption) {
+      return res
+        .status(400)
+        .send({ message: "Caption is required or malformed." });
     }
+
+    if (!fileName) {
+      return res.status(400).send({ message: "File url is required." });
+    }
+
+    const item = await new FeedItem({
+      caption: caption,
+      url: fileName,
+    });
+
+    const savedItem = await item.save();
+
+    savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+    res.status(201).send(savedItem);
   });
-  res.send(items);
-});
 
 // Get a feed resource
-router.get('/:id',
-    async (req: Request, res: Response) => {
-      const {id} = req.params;
-      const item = await FeedItem.findByPk(id);
-      res.send(item);
-    });
+router.get("/:id", async (req: Request, res: Response) => {
+  console.log("feed 2 get called");
+  const { id } = req.params;
+  const item = await FeedItem.findByPk(id);
+  res.send(item);
+});
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const {fileName} = req.params;
-      const url = AWS.getPutSignedUrl(fileName);
-      res.status(201).send({url: url});
-    });
-
-// Create feed with metadata
-router.post('/',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const caption = req.body.caption;
-      const fileName = req.body.url; // same as S3 key name
-
-      if (!caption) {
-        return res.status(400).send({message: 'Caption is required or malformed.'});
-      }
-
-      if (!fileName) {
-        return res.status(400).send({message: 'File url is required.'});
-      }
-
-      const item = await new FeedItem({
-        caption: caption,
-        url: fileName,
-      });
-
-      const savedItem = await item.save();
-
-      savedItem.url = AWS.getGetSignedUrl(savedItem.url);
-      res.status(201).send(savedItem);
-    });
+router.get(
+  "/signed-url/:fileName",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    console.log("feed 3 get called");
+    const { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({ url: url });
+  }
+);
 
 export const FeedRouter: Router = router;
