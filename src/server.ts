c3d7f45bd57261708fdb1425bd449a1ea93a4867
Waqsa Noor <waqasn2c@gmail.com>
2020-12-21 00:37:25 +0500

    feat: allowed cors support for all origin

--- src/server.ts
+++ src/server.ts
@@ -32,6 +32,15 @@ import { IndexRouter } from "./controllers/v0/index.router";
   //     origin: config.url,
   //   })
   // );
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
   app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
